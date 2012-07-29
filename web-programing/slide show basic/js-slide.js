21c5526cd839c9892bbde14b1b9b98b9555e150c
s-soodmand <somaieh.soodmand@gmail.com>
2012-07-30 00:15:11 +0330

    slide-show basic-2 [js]

--- web-programing/slide show basic/js-slide.js
+++ web-programing/slide show basic/js-slide.js
@@ -2,9 +2,12 @@ window.onload=function(){
 	var slideshow = document.getElementsByClassName('slideshow').item(0);
 	var list = slideshow.getElementsByClassName('button').item(0).getElementsByTagName('li');
 	for(var i=0;i<list.length;i++){
-		list.item(i).onclick=function(){
-			alert(i);
-		}
+		(function(j){
+			list.item(j).onclick=function(){
+				alert(j);
+			}	
+		})(i);
+		
 	}
 
 }

c76cad08971d6f8f86d1c614bdf582e0c0b7eabe
s-soodmand <somaieh.soodmand@gmail.com>
2012-09-01 15:47:55 +0330

    Theme for Wordpress

---
+++ web-programing/worpress/MyTheme/header.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"[]>
+<html xmlns="http://www.w3.org/1999/xhtml" dir="ltr" lang="en-US" xml:lang="en">
+<head>
+    <!--
+    Created by Artisteer v3.1.0.46836
+    Base template (without user's data) checked by http://validator.w3.org : "This page is valid XHTML 1.0 Transitional"
+    -->
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <title>Home</title>
+
+
+
+    <link rel="stylesheet" href="<?php bloginfo('template_url');?>/style.css" type="text/css" media="screen" />
+    <!--[if IE 6]><link rel="stylesheet" href="style.ie6.css" type="text/css" media="screen" /><![endif]-->
+    <!--[if IE 7]><link rel="stylesheet" href="style.ie7.css" type="text/css" media="screen" /><![endif]-->
+ 
+</head>
+<body>
+<div id="art-main">
+    <div class="cleared reset-box"></div>
+		<div class="art-bar art-nav">
+			<div class="art-nav-outer">
+				<div class="art-nav-wrapper">
+					<div class="art-nav-inner">
+                        <ul class="art-hmenu">
+                            <li>
+                                <a href="./" class="active">Home</a>
+                            </li>	
+                            <li>
+                                <a href="#">Contact Me</a>
+                            </li>	
+                        </ul>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="cleared reset-box"></div>
+		<div class="art-header">
+        	<div class="art-header-position">
+           		<div class="art-header-wrapper">
+                    <div class="cleared reset-box"></div>
+                    <div class="art-header-inner">
+                    	<div class="art-headerobject"></div>
+                        <div class="art-logo">
+                            <h1 class="art-logo-name"><a href="./index.html">Photica</a></h1>
+                            <h2 class="art-logo-text">My Memorable Works</h2>
+                        </div>
+                	</div>
+            	</div>
+        	</div>
+    	</div>
+    	<div class="cleared reset-box"></div>
+    	<div class="art-box art-sheet">
+        	<div class="art-box-body art-sheet-body">
---
+++ web-programing/worpress/MyTheme/index.php
@@ -0,0 +1,11 @@
+<?php 
+	include('header.php'); 
+	include('menu-top.php');
+	include('content.php');
+ 	include('menu-right.php');
+	include('sidebar.php');
+	include('footer.php');
+?>
+									
+								
+    	
